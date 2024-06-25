9e9c3a68848fec2b6dce46eb2537ef171f14c11c
0xb <113536612+kingbaldwin-iv@users.noreply.github.com>
2024-06-25 17:32:06 +0200

    Add files via upload

---
+++ logging_with_DL.hs
@@ -0,0 +1,29 @@
+type DList a = [a] -> [a]
+
+empty :: DList a
+empty = \xs -> xs -- returns a lambda that returns the lists itself
+
+sngl :: a -> DList a
+sngl x = \xs -> x : xs -- returns a lambda that appends x to the supplied list xs to the lambda
+
+app :: DList a -> DList a -> DList a
+ys `app` zs = \xs -> ys (zs xs) -- returns a lambda that gets a list and supply it to lambda that gets a list which then supply it to another lambda that gets a list 
+
+fromList :: [a] -> DList a
+fromList ys = \xs -> ys ++ xs -- returns a lambda that gets a list and append it to the list that was previously supplied
+
+toList :: DList a -> [a]
+toList ys = ys [] -- applies the series of lambdas to empty list
+
+type DLog = DList (Integer,Integer)
+
+gcdLog :: Integer -> Integer -> DLog -> (Integer, DLog)
+gcdLog a b dlog
+ | a == b = (a,log')
+ | a > b = gcdLog (a-b) b log'
+ | otherwise = gcdLog a (b-a) log'
+ where
+  log' = dlog `app` sngl (a,b)
+
+gcdGetLog :: Integer -> Integer -> [(Integer,Integer)]
+gcdGetLog a b = toList (snd (gcdLog a b empty))
